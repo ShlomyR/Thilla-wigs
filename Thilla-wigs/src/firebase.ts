1232f828a7305b124bb598475ebe09c3bc77e22d
ShlomyR <rshlomy@gmail.com>
2023-08-13 11:30:32 +0300

    fixed untracked files

--- Thilla-wigs/src/firebase.ts
+++ Thilla-wigs/src/firebase.ts
@@ -4,23 +4,36 @@ import { getFirestore } from "firebase/firestore";
 import { initializeAppCheck } from "firebase/app-check";
 import { getPerformance } from "firebase/performance";
 
+
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../thilla-wigs-firebase-adminsdk-i7sve-057422fdfb.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://thilla-wigs.firebaseapp.com' // Replace with your Firebase project URL
+// });
+
 const firebaseConfig = {
-    apiKey: "you'r key",
-    authDomain: "you'r key",
-    projectId: "you'r key",
-    storageBucket: "you'r key",
-    messagingSenderId: "you'r key",
-    appId: "you'r key"
+    apiKey: "AIzaSyAvTVDmzxT4M-Mc0kSbKpHphVxup5U8c3U",
+    authDomain: "thilla-wigs.firebaseapp.com",
+    projectId: "thilla-wigs",
+    storageBucket: "thilla-wigs.appspot.com",
+    messagingSenderId: "421646576130",
+    appId: "1:421646576130:web:4dc4b76b3b6c72d24f151b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 
+if (import.meta.env.DEV) {
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+
 import("firebase/app-check")
   .then(({ ReCaptchaV3Provider }) => {
     initializeAppCheck(app, {
-      provider: new ReCaptchaV3Provider("you'r key of the reCaptcha here"),
+      provider: new ReCaptchaV3Provider('6LeJF2EnAAAAAFKF4ifZWj6314bFt-AhKwqvv9bg'),
       isTokenAutoRefreshEnabled: true,
     });
   });
@@ -30,3 +43,11 @@ export const perf = getPerformance(app);
 export const auth = getAuth();
 
 export const db = getFirestore(app);
+
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("../thilla-wigs-firebase-adminsdk-i7sve-057422fdfb.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
